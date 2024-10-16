c8d8d82f508aeeb7f9b3a1417932759226e080dc
Vishal Verma <160992851+vishal-verma-96@users.noreply.github.com>
2024-10-17 04:50:00 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -41,7 +41,7 @@ st.markdown(
         z-index: 1; 
         display: flex; 
         align-items: center; 
-        padding: 0 120px;}}
+        padding: 0 80px;}}
     .header h1
     {{
         color: White; 
